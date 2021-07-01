58083d4d891a0dba084115d1d9e57dac71591dc9
guanronlin <guanronlin@tencent.com>
2021-07-01 18:00:42 +0800

    // 4) Random function

--- GLSLRayMarching/GLSLRayMarching/MacShaderDemo/Demos/PathTracings/Course/9 Step By Step/buffera.glsl
+++ GLSLRayMarching/GLSLRayMarching/MacShaderDemo/Demos/PathTracings/Course/9 Step By Step/buffera.glsl
@@ -7,10 +7,27 @@
 #define MAX_DEPTH 64
 
 ////////////////////////////////////////////////////////////////////////////////////////
+// Util functions
+// 4) Random function
+float seed = 0.0;
+float rand() 
+{ 
+    return fract(sin(seed++)*43758.5453123); 
+}
+
+void rand_seek(in vec2 fragCoord)
+{
+    seed = iTime + iResolution.y * fragCoord.x / iResolution.x + fragCoord.y / iResolution.y;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
 // 1) main function
 void mainImage( out vec4 fragColor, in vec2 fragCoord ) 
 {
+	// 4) Random function
+	rand_seek(fragCoord);
+
 	// 3) Test Color Output
 	vec2 uv = fragCoord.xy / iResolution.xy;
-	fragColor = vec4(uv, 0.0, 1.0);
+	fragColor = vec4(uv * vec2(rand(), rand()), 0.0, 1.0);
 }
