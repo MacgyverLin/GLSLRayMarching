ffacd0e7756f3ff24769bb20e729065569d1e8c4
guanronlin <guanronlin@tencent.com>
2021-07-01 17:56:40 +0800

    // 3) Test Color Output

--- GLSLRayMarching/GLSLRayMarching/MacShaderDemo/Demos/PathTracings/Course/9 Step By Step/buffera.glsl
+++ GLSLRayMarching/GLSLRayMarching/MacShaderDemo/Demos/PathTracings/Course/9 Step By Step/buffera.glsl
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////////////
-// Constants
+// 2) Constants
 #define PI 3.14159265359
 #define EPSILON 1e-4
 #define RAY_EPSILON 1e-3
@@ -10,5 +10,7 @@
 // 1) main function
 void mainImage( out vec4 fragColor, in vec2 fragCoord ) 
 {
-	fragColor = vec4(0.0, 0.0, 0.0, 1.0);
+	// 3) Test Color Output
+	vec2 uv = fragCoord.xy / iResolution.xy;
+	fragColor = vec4(uv, 0.0, 1.0);
 }
