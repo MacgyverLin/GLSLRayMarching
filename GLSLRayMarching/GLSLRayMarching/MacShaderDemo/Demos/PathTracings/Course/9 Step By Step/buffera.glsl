9c4673193bca70e71fc75c768c517364fcbfa64e
guanronlin <guanronlin@tencent.com>
2021-07-02 12:09:56 +0800

    2) rand_seek, rand, rand color

--- GLSLRayMarching/GLSLRayMarching/MacShaderDemo/Demos/PathTracings/Course/9 Step By Step/buffera.glsl
+++ GLSLRayMarching/GLSLRayMarching/MacShaderDemo/Demos/PathTracings/Course/9 Step By Step/buffera.glsl
@@ -6,7 +6,22 @@
 #define SUB_SAMPLES 1
 #define MAX_DEPTH 64
 
+////////////////////////////////////////////////////////////////////////////////////////
+// Util functions
+float seed = 0.;
+float rand() 
+{ 
+    return fract(sin(seed++)*43758.5453123); 
+}
+
+void rand_seek(in vec2 fragCoord)
+{
+    seed = iTime + iResolution.y * fragCoord.x / iResolution.x + fragCoord.y / iResolution.y;
+}
+
 void mainImage( out vec4 fragColor, in vec2 fragCoord ) 
 {
-    fragColor = vec4(0.0, 1.0, 0.0, 1.0);
+    rand_seek(fragCoord);
+
+    fragColor = vec4(rand(), rand(), rand() , 1.0);
 }
