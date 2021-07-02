85a6423df000e63400d7efd8c290c0a745882847
guanronlin <guanronlin@tencent.com>
2021-07-02 12:08:21 +0800

    1) add main, add some constant

---
+++ GLSLRayMarching/GLSLRayMarching/MacShaderDemo/Demos/PathTracings/Course/9 Step By Step/buffera.glsl
@@ -0,0 +1,12 @@
+////////////////////////////////////////////////////////////////////////////////////////
+// Constants
+#define PI 3.14159265359
+#define EPSILON 1e-4
+#define RAY_EPSILON 1e-3
+#define SUB_SAMPLES 1
+#define MAX_DEPTH 64
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord ) 
+{
+    fragColor = vec4(0.0, 1.0, 0.0, 1.0);
+}
