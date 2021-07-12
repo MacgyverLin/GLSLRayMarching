6b62753a8ae9e2231a4d6bead443dae8c34ce59b
guanronlin <guanronlin@tencent.com>
2021-07-12 18:34:48 +0800

    sad

--- GLSLRayMarching/GLSLRayMarching/MacShaderDemo/Demos/PathTracings/Course/10 Step By Step/image.glsl
+++ GLSLRayMarching/GLSLRayMarching/MacShaderDemo/Demos/PathTracings/Course/10 Step By Step/image.glsl
@@ -6,21 +6,6 @@
 int seed;
 int flat_idx;
 
-const float light_size = 0.5;
-const float light_area = light_size * light_size;
-const vec3 light_position = vec3(0.0, 0.90, 0.5);
-const vec3 light_normal = vec3(0, -1, 0);
-const vec4 light_albedo = vec4(1, 1, 1, 2.0 / (light_size * light_size));
-
-struct HitRecord
-{
-	bool hit;
-	float t;
-	vec3 position;
-	vec3 normal;
-	vec4 albedo;
-};
-
 ///////////////////////////////////////////////////////
 // random
 void encrypt_tea(inout uvec2 arg)
@@ -108,11 +93,136 @@ vec3 ray_at(in Ray ray, float t)
 	return ray.origin + t * ray.dir;
 }
 
+struct Transform
+{
+	vec3 position;
+	vec3 rotation;
+};
+
+vec3 transformPosition(vec3 p)
+{
+	return vec3(0.0);
+}
+
+vec3 transformDir(vec3 p)
+{
+	return vec3(0.0);
+}
+
+////////////////////////////////////////////////////
+struct HitRecord
+{
+	bool hit;
+	float t;
+	vec3 position;
+	vec3 normal;
+	vec4 albedo;
+};
+
+////////////////////////////////////////////////////
+// Plane
+struct Plane
+{
+	Transform transform;
+	vec3 normal;
+	vec2 size;
+
+	vec4 albedo;
+
+	int major_axis;
+};
+
+void intersect_plane(in Ray ray, in Plane plane, out HitRecord hitrecord)
+{
+    float denom = dot(ray.dir, plane.normal);
+    
+	float t = dot(plane.transform.position - ray.origin, plane.normal) / denom;
+
+	if(t > 0.0)
+	{
+		vec3 p_tmp = ray_at(ray, t);
+
+		vec2 p_swizzle;
+		if(plane.major_axis==0)
+			p_swizzle = p_tmp.yz; // x major
+		else if(plane.major_axis==1)
+			p_swizzle = p_tmp.xz; // y major
+		else
+			p_swizzle = p_tmp.xy; // z major
+
+		if(all(lessThanEqual(p_swizzle, vec2(1))) && all(greaterThanEqual(p_swizzle, vec2(-1))))
+		{
+			hitrecord.hit		= true;
+			hitrecord.t			= t;
+			hitrecord.position	= p_tmp;
+			hitrecord.normal	= plane.normal;
+			hitrecord.albedo	= plane.albedo;
+		}
+		else
+		{
+			hitrecord.hit		= false;
+			hitrecord.t			= INFINITY;
+		}
+	}
+	else
+	{
+		hitrecord.hit		= false;
+		hitrecord.t			= INFINITY;
+	}
+}
+
+/////////////////////////////////////////////////////
+// Light
+struct Light
+{
+	Transform transform;
+	vec3 normal;
+	float size;
+
+	vec4 albedo;
+};
+
+vec3 randomSampleLight(in Light light)
+{
+	vec2 rng = get_random();
+	return light.transform.position + vec3(rng.x - 0.5, 0, rng.y - 0.5) * light.size;
+}
+
+void intersect_light(in Ray ray, in Light light, out HitRecord hitrecord)
+{
+    float denom = dot(ray.dir, light.normal);
+	float t = dot(light.transform.position - ray.origin, light.normal) / denom;
+
+	vec3 p_tmp = ray_at(ray, t);
+	if(t > 0.0)
+	{
+		if(all(lessThan(abs(light.transform.position - p_tmp).xz, vec2(light.size * 0.5)))) 
+		{
+			hitrecord.hit		= true;
+			hitrecord.t			= t;
+			hitrecord.position	= p_tmp;
+			hitrecord.normal	= light.normal;
+			hitrecord.albedo	= light.albedo;
+		}
+		else
+		{
+			hitrecord.hit		= false;
+			hitrecord.t			= INFINITY;
+		}
+	}
+	else
+	{
+		hitrecord.hit		= false;
+		hitrecord.t			= INFINITY;
+	}
+}
+
 /////////////////////////////////////////////////////
 // AABB
 struct AABB
 {
-	vec3 min_, max_;
+	vec3 min_;
+	vec3 max_;
 };
 
 bool intersect_aabb(in Ray ray, in AABB aabb, inout float t_min, inout float t_max)
@@ -130,215 +240,110 @@ bool intersect_aabb(in Ray ray, in AABB aabb, inout float t_min, inout float t_m
 	return t_min < t_max;
 }
 
-////////////////////////////////////////////////////
-// Plane
-float intersect_plane(Ray ray, vec3 center, vec3 normal)
-{
-    float denom = dot(ray.dir, normal);
-    float t = dot(center - ray.origin, normal) / denom;
-	return t > 0.0 ? t : INFINITY;
-}
-
 /////////////////////////////////////////////////////
 // Box
-float intersect_box(Ray ray, out vec3 normal, vec3 size)
+struct Box
 {
+	Transform transform;
+	vec3 size;
+
+	vec4 albedo;
+};
+
+void intersect_box(in Ray ray, in Box box, out HitRecord hitrecord)
+{
+	Ray ray_tmp			= ray;
+	mat4 r				= rotate_y(box.transform.rotation[1]);
+	ray_tmp.origin		-= box.transform.position;
+	ray_tmp.origin		= vec3(r * vec4(ray_tmp.origin, 1.0));
+	ray_tmp.dir			= vec3(r * vec4(ray_tmp.dir, 0));
+
 	float t_min = 0.0;
 	float t_max = 999999999.0;
-	if(intersect_aabb(ray, AABB(-size, size), t_min, t_max)) 
+	if(intersect_aabb(ray_tmp, AABB(-box.size, box.size), t_min, t_max)) 
 	{
-		vec3 p = ray_at(ray, t_min);
-		p /= size;
+		vec3 p = ray_at(ray_tmp, t_min);
+		p /= box.size;
 		if(abs(p.x) > abs(p.y)) 
 		{
 			if(abs(p.x) > abs(p.z)) 
 			{
-				normal = vec3(p.x > 0.0 ? 1.0 : -1.0, 0, 0);
+				hitrecord.normal = vec3(p.x > 0.0 ? 1.0 : -1.0, 0, 0);
 			}
 			else 
 			{
-				normal = vec3(0, 0, p.z > 0.0 ? 1.0 : -1.0);
+				hitrecord.normal = vec3(0, 0, p.z > 0.0 ? 1.0 : -1.0);
 			}
 		}
 		else if(abs(p.y) > abs(p.z)) 
 		{
-			normal = vec3(0, p.y > 0.0 ? 1.0 : -1.0, 0);
+			hitrecord.normal = vec3(0, p.y > 0.0 ? 1.0 : -1.0, 0);
 		}
 		else 
 		{
-			normal = vec3(0, 0, p.z > 0.0 ? 1.0 : -1.0);
+			hitrecord.normal = vec3(0, 0, p.z > 0.0 ? 1.0 : -1.0);
 		}
 
-		return t_min;
+		hitrecord.hit		= true;
+		hitrecord.t			= t_min;
+		hitrecord.position	= ray_at(ray, hitrecord.t);
+		hitrecord.normal	= vec3(transpose(r) * vec4(hitrecord.normal, 0.0));
+		hitrecord.albedo	= box.albedo;
 	}
-
-	return INFINITY;
-}
-
-/////////////////////////////////////////////////////
-// Light
-vec3 sample_light(vec2 rng)
-{
-	return light_position + vec3(rng.x - 0.5, 0, rng.y - 0.5) * light_size;
-}
-
-float intersect_light(Ray ray)
-{
-	float t = intersect_plane(ray, light_position, light_normal);
-
-	vec3 p = ray_at(ray, t);
-	if(all(lessThan(abs(light_position - p).xz, vec2(light_size * 0.5)))) 
+	else
 	{
-		return t;
+		hitrecord.hit		= false;
+		hitrecord.t			= INFINITY;
 	}
-
-	return INFINITY;
 }
 
 ///////////////////////////////////////////////////////////////////
-bool intersect(in Ray ray, inout HitRecord hitrecord)
+// Scene Description
+#define NUM_BOXES 2
+Box boxes[NUM_BOXES];
+
+#define NUM_LIGHTS 1
+Light lights[NUM_LIGHTS];
+
+#define NUM_PLANES 5
+Plane planes[NUM_PLANES];
+
+///////////////////////////////////////////////////////////////////
+void intersect(in Ray ray, inout HitRecord hitrecord)
 {
 	hitrecord.hit		= false;
 	hitrecord.t			= INFINITY;
 	hitrecord.albedo	= vec4(0.0);
 
+	for(int i=0; i<NUM_LIGHTS; i++)
 	{
-		float t = intersect_light(ray);
-		if(t < hitrecord.t) 
+		HitRecord lightHitrecord;
+		intersect_light(ray, lights[i], lightHitrecord);
+		if(lightHitrecord.t < hitrecord.t) 
 		{
-			hitrecord.hit		= true;
-			hitrecord.t			= t;
-			hitrecord.position	= ray_at(ray, hitrecord.t);
-			hitrecord.normal	= light_normal;
-			hitrecord.albedo	= light_albedo;
+			hitrecord = lightHitrecord;
 		}
 	}
 
+	for(int i=0; i<NUM_BOXES; i++)
 	{
-		vec3 normal_tmp;
-		Ray ray_tmp = ray;
-		mat4 r = rotate_y(0.3);
-		ray_tmp.origin -= vec3(-0.35, -0.5, -0.35);
-		ray_tmp.dir = vec3(r * vec4(ray_tmp.dir, 0));
-		ray_tmp.origin = vec3(r * vec4(ray_tmp.origin, 1.0));
-		float t = intersect_box(ray_tmp, normal_tmp, vec3(0.25, 0.5, 0.25));
-
-		if(t < hitrecord.t) 
+		HitRecord boxHitrecord;
+		intersect_box(ray, boxes[i], boxHitrecord);
+		if(boxHitrecord.t < hitrecord.t) 
 		{
-			hitrecord.hit		= true;
-			hitrecord.t			= t;
-			hitrecord.position	= ray_at(ray, hitrecord.t);
-			hitrecord.normal	= vec3(transpose(r) * vec4(normal_tmp, 0.0));
-			hitrecord.albedo	= vec4(0.7, 0.7, 0.7, 0);
+			hitrecord = boxHitrecord;
 		}
 	}
 
+	for(int i=0; i<NUM_PLANES; i++)
 	{
-		vec3 normal_tmp;
-		Ray ray_tmp = ray;
-		ray_tmp.origin -= vec3(0.5, -0.75, 0.35);
-		float t = intersect_box(ray_tmp, normal_tmp, vec3(0.25, 0.25, 0.25));
-		if(t < hitrecord.t) 
+		HitRecord planeHitrecord;
+		intersect_plane(ray, planes[i], planeHitrecord);
+		if(planeHitrecord.t < hitrecord.t) 
 		{
-			hitrecord.hit		= true;
-			hitrecord.t			= t;
-			hitrecord.position	= ray_at(ray, hitrecord.t);
-			hitrecord.normal	= normal_tmp;
-			hitrecord.albedo	= vec4(0.7, 0.7, 0.7, 0);
+			hitrecord = planeHitrecord;
 		}
 	}
-
-	// left
-	{
-		vec3 n = vec3(1, 0, 0);
-		float t = intersect_plane(ray, vec3(-1, 0, 0), n);
-		if(t < hitrecord.t) 
-		{
-			vec3 p_tmp = ray_at(ray, t);
-			if(all(lessThanEqual(p_tmp.yz, vec2(1))) && all(greaterThanEqual(p_tmp.yz, vec2(-1))))
-			{
-				hitrecord.hit		= true;
-				hitrecord.t			= t;
-				hitrecord.position	= ray_at(ray, t);
-				hitrecord.normal	= n;
-				hitrecord.albedo	= vec4(0.9, 0.1, 0.1, 0);
-			}
-		}
-	}
-
-	// right
-	{
-		vec3 n = vec3(-1, 0, 0);
-		float t = intersect_plane(ray, vec3(1, 0, 0), n);
-		if(t < hitrecord.t)
-		{
-			vec3 p_tmp = ray_at(ray, t);
-			if(all(lessThanEqual(p_tmp.yz, vec2(1))) && all(greaterThanEqual(p_tmp.yz, vec2(-1))))
-			{
-				hitrecord.hit		= true;
-				hitrecord.t			= t;
-				hitrecord.position	= p_tmp;
-				hitrecord.normal	= n;
-				hitrecord.albedo	= vec4(0.1, 0.9, 0.1, 0);
-			}
-		}
-	}
-
-	// floor
-	{
-		vec3 n = vec3(0, 1, 0);
-		float t = intersect_plane(ray, vec3(0, -1, 0), n);
-		if(t < hitrecord.t) 
-		{
-			vec3 p_tmp = ray_at(ray, t);
-			if(all(lessThan(p_tmp.xz, vec2(1))) && all(greaterThan(p_tmp.xz, vec2(-1))))
-			{
-				hitrecord.hit		= true;
-				hitrecord.t			= t;
-				hitrecord.position	= p_tmp;
-				hitrecord.normal	= n;
-				hitrecord.albedo	= vec4(0.7, 0.7, 0.7, 0);
-			}
-		}
-	}
-
-	// ceiling
-	{
-		vec3 n = vec3(0, -1, 0);
-		float t = intersect_plane(ray, vec3(0, 1, 0), n);
-		if(t < hitrecord.t) 
-		{
-			vec3 p_tmp = ray_at(ray, t);
-			if(all(lessThan(p_tmp.xz, vec2(1))) && all(greaterThan(p_tmp.xz, vec2(-1))))
-			{
-				hitrecord.hit		= true;
-				hitrecord.t			= t;
-				hitrecord.position	= p_tmp;
-				hitrecord.normal	= n;
-				hitrecord.albedo	= vec4(0.7, 0.7, 0.7, 0);
-			}
-		}
-	}
-
-	// back wall
-	{
-		vec3 n = vec3(0, 0, 1);
-		float t = intersect_plane(ray, vec3(0, 0, -1), n);
-		if(t < hitrecord.t) 
-		{
-			vec3 p_tmp = ray_at(ray, t);
-			if(all(lessThan(p_tmp.xy, vec2(1))) && all(greaterThan(p_tmp.xy, vec2(-1))))
-			{
-				hitrecord.hit		= true;
-				hitrecord.t			= t;
-				hitrecord.position	= p_tmp;
-				hitrecord.normal	= n;
-				hitrecord.albedo	= vec4(0.7, 0.7, 0.7, 0);
-			}
-		}
-	}
-
-	return hitrecord.hit;
 }
 
 bool test_visibility(vec3 p1, vec3 p2)
@@ -354,34 +359,42 @@ bool test_visibility(vec3 p1, vec3 p2)
 	return hitrecord.t > distance(p1, p2) - 2.0 * eps;
 }
 
-void sampleLight(inout HitRecord hitrecord, inout vec3 radiance, inout vec3 throughput)
+float getLightArea(in Light light)
+{
+	return light.size * light.size;
+}
+
+vec3 getLightLe(in Light light)
+{
+	return light.albedo.rgb * light.albedo.a;
+}
+
+void sampleLight(in Light light, inout HitRecord hitrecord, inout vec3 radiance, inout vec3 throughput)
 { 
 	/* NEE */
-	vec3 pos_ls = sample_light(get_random());
+	vec3 pos_ls = randomSampleLight(light);
 
 	vec3 l_nee = pos_ls - hitrecord.position;
 	float rr_nee = dot(l_nee, l_nee);
 	l_nee /= sqrt(rr_nee);
-	float G = max(0.0, dot(hitrecord.normal, l_nee)) * max(0.0, -dot(light_normal, l_nee)) / rr_nee;
+	float G = max(0.0, dot(hitrecord.normal, l_nee)) * max(0.0, -dot(light.normal, l_nee)) / rr_nee;
 
 	if(G > 0.0) 
 	{
-		float light_pdf = 1.0 / (light_area * G);
+		float light_pdf = 1.0 / (getLightArea(light) * G);
 		float brdf_pdf = 1.0 / PI;
-
 		float w = light_pdf / (light_pdf + brdf_pdf);
-
 		vec3 brdf = hitrecord.albedo.rgb / PI;
 
 		if(test_visibility(hitrecord.position, pos_ls)) 
 		{
-			vec3 Le = light_albedo.rgb * light_albedo.a;
+			vec3 Le = getLightLe(light);
 			radiance += w * (throughput * (Le * brdf) / light_pdf);
 		}
 	}
 }
 
-bool sampleBRDF(inout HitRecord hitrecord, inout vec3 radiance, inout vec3 throughput)
+bool sampleBRDF(in Light light, inout HitRecord hitrecord, inout vec3 radiance, inout vec3 throughput)
 { 
 	/* brdf */
 	mat3 onb = construct_ONB_frisvad(hitrecord.normal);
@@ -398,35 +411,30 @@ bool sampleBRDF(inout HitRecord hitrecord, inout vec3 radiance, inout vec3 throu
 	if(!hitrecordNext.hit)
 		return false;
 
-	if(hitrecordNext.albedo.a > 0.0)  /* if hit a light */
+	if(hitrecordNext.albedo.a > 0.0)	// if hit a light
 	{ 
 		float G = max(0.0, dot(rayNext.dir / hitrecordNext.t, hitrecord.normal)) * max(0.0, -dot(rayNext.dir / hitrecordNext.t, hitrecordNext.normal));
-		if(G > 0.0) /* if hit back side of light source */
+		if(G > 0.0)						// if hit back side of light source
 		{
 			return false;
 		}
 		else
 		{
-			vec3 brdf = hitrecord.albedo.rgb / PI;
-
 			float brdf_pdf = 1.0 / PI;
-			float light_pdf = 1.0 / (light_area * G);
-
+			float light_pdf = 1.0 / (getLightArea(light) * G);
 			float w = brdf_pdf / (light_pdf + brdf_pdf);
-
-			vec3 Le = light_albedo.rgb * light_albedo.a;
+			vec3 brdf = hitrecord.albedo.rgb / PI;
+			vec3 Le = getLightLe(light);
 
 			radiance += w * (throughput * (Le * brdf) / brdf_pdf);
 
 			return false;
 		}
 	}
-	else /* if hit an object */
+	else								// if hit an object
 	{
 		vec3 brdf = hitrecord.albedo.rgb / PI;
-
 		float brdf_pdf = 1.0 / PI;
-
 		throughput *= brdf / brdf_pdf;
 
 		hitrecord = hitrecordNext;
@@ -442,26 +450,26 @@ vec3 traceWorld(Ray ray)
 
 	HitRecord hitrecord;
 	intersect(ray, hitrecord);
-	if(hitrecord.hit) // hit shader
+	if(hitrecord.hit)					// hit shader
 	{
-		if(hitrecord.albedo.a > 0.0) /* if hit a light */
+		if(hitrecord.albedo.a > 0.0)	// if hit a light
 		{ 
 			return hitrecord.albedo.rgb * hitrecord.albedo.a;
 		}
-		else /* if hit an object */
+		else							// if hit an object
 		{
 			for(int i = 0; i < NUM_BOUNCES; i++) 
 			{
-				sampleLight(hitrecord, radiance, throughput);
+				sampleLight(lights[0], hitrecord, radiance, throughput);
 
-				if( !sampleBRDF(hitrecord, radiance, throughput) )
+				if( !sampleBRDF(lights[0], hitrecord, radiance, throughput) )
 					break;
 			}
 	
 			return radiance;
 		}
 	}
-	else // miss shader
+	else								// miss shader
 	{
 		return vec3(0.0);
 	}
@@ -473,6 +481,23 @@ void init()
 	seed = 0;
 
 	rand_seed();
+
+	boxes[0] = Box( Transform(vec3(-0.35, -0.50, -0.35), vec3(0.0, 0.3, 0.0)), vec3(0.25, 0.50, 0.25),  vec4(0.7, 0.7, 0.7, 0) );
+	boxes[1] = Box( Transform(vec3( 0.50, -0.75,  0.35), vec3(0.0, 0.0, 0.0)), vec3(0.25, 0.25, 0.25),  vec4(0.7, 0.7, 0.7, 0) );
+
+	const float light_size = 0.5;
+	const float light_area = light_size * light_size;
+	const vec3 light_position = vec3(0.0, 0.90, 0.5);
+	const vec3 light_normal = vec3(0, -1, 0);
+	const vec4 light_albedo = vec4(1, 1, 1, 2.0 / (light_size * light_size));
+
+	lights[0] = Light( Transform(vec3(light_position), vec3(0, 0, 0)), light_normal, light_size, light_albedo );
+
+	planes[0] = Plane(Transform(vec3(-1,  0,  0), vec3(0, 0, 0)), vec3( 1,  0,  0), vec2(1, 1), vec4(0.9, 0.1, 0.1, 0), 0);
+	planes[1] = Plane(Transform(vec3( 1,  0,  0), vec3(0, 0, 0)), vec3(-1,  0,  0), vec2(1, 1), vec4(0.1, 0.9, 0.1, 0), 0);
+	planes[2] = Plane(Transform(vec3( 0, -1,  0), vec3(0, 0, 0)), vec3( 0,  1,  0), vec2(1, 1), vec4(0.7, 0.7, 0.7, 0), 1);
+	planes[3] = Plane(Transform(vec3( 0,  1,  0), vec3(0, 0, 0)), vec3( 0, -1,  0), vec2(1, 1), vec4(0.7, 0.7, 0.7, 0), 1);
+	planes[4] = Plane(Transform(vec3( 0,  0, -1), vec3(0, 0, 0)), vec3( 0,  0,  1), vec2(1, 1), vec4(0.7, 0.7, 0.7, 0), 2);
 }
 
 struct Camera
