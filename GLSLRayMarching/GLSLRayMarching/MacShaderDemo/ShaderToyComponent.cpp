4fc76563d3eb138cc73404ba0d6312570ad7de57
guanronlin <guanronlin@tencent.com>
2022-10-20 12:50:14 +0800

    change scene

--- GLSLRayMarching/GLSLRayMarching/MacShaderDemo/ShaderToyComponent.cpp
+++ GLSLRayMarching/GLSLRayMarching/MacShaderDemo/ShaderToyComponent.cpp
@@ -103,7 +103,7 @@ bool ShaderToyComponent::OnStart()
 	//return shaderToyRenderer->Initiate("Demos/Scattering/RealySimpleAtmosphericScatter");
 	//return shaderToyRenderer->Initiate("Demos/Terrains/Cloudy Terrain");
 	//return shaderToyRenderer->Initiate("Demos/Terrains/Desert Sand");
-	////return shaderToyRenderer->Initiate("Demos/Terrains/Elevated");
+	return shaderToyRenderer->Initiate("Demos/Terrains/Elevated");
 	////return shaderToyRenderer->Initiate("Demos/Terrains/Lake in highland");
 	////return shaderToyRenderer->Initiate("Demos/Terrains/Mountains");
 	////return shaderToyRenderer->Initiate("Demos/Terrains/Rainforest");
