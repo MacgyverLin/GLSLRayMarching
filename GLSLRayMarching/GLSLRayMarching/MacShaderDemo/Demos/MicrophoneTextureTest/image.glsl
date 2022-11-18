cdf2a22e949a2c0faa9226dd4346fb0c29f4ddd1
guanronlin <guanronlin@tencent.com>
2022-11-18 23:15:54 +0800

    add demos

---
+++ GLSLRayMarching/GLSLRayMarching/MacShaderDemo/Demos/MicrophoneTextureTest/image.glsl
@@ -0,0 +1,9 @@
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{  
+    vec2 uv = (fragCoord) / iResolution.xy;
+
+    vec3 voice = texture(iChannel0, vec2(uv.x*10.0, 0.0)).rgb;
+    uv.y += voice.x*0.1;
+
+    fragColor.xyz = texture(iChannel1, uv).rgb;
+}
